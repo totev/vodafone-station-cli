24fcdf1b7c64054f86425cee4a44c2f5d1e53b28
Dobroslav Totev <dev@totev.de>
2022-03-27 19:18:14 +0200

    chore: increase discovery time for unresponsive modems

--- src/modem/discovery.ts
+++ src/modem/discovery.ts
@@ -4,7 +4,7 @@ import { TechnicolorConfiguration } from './technicolor-modem'
 import { extractFirmwareVersion } from './tools/html-parser'
 const BRIDGED_MODEM_IP = '192.168.100.1'
 const ROUTER_IP = '192.168.0.1'
-axios.defaults.timeout = 1000
+axios.defaults.timeout = 10000
 
 export async function discoverModemIp(): Promise<string> {
   try {
@@ -22,17 +22,17 @@ export async function discoverModemIp(): Promise<string> {
   }
 }
 
-export interface ModemInformation{
+export interface ModemInformation {
   deviceType: 'Arris' | 'Technicolor';
   firmwareVersion: string;
   ipAddress: string;
 }
 
 export class ModemDiscovery {
-  constructor(private readonly modemIp: string, private readonly logger: Log) {}
+  constructor(private readonly modemIp: string, private readonly logger: Log) { }
 
   async tryTechnicolor(): Promise<ModemInformation> {
-    const {data} = await axios.get<TechnicolorConfiguration>(`http://${this.modemIp}/api/v1/login_conf`)
+    const { data } = await axios.get<TechnicolorConfiguration>(`http://${this.modemIp}/api/v1/login_conf`)
     this.logger.debug(`Technicolor login configuration: ${JSON.stringify(data)}`)
     if (data.error === 'ok' && data.data?.firmwareversion) {
       return {
@@ -45,8 +45,9 @@ export class ModemDiscovery {
   }
 
   async tryArris(): Promise<ModemInformation> {
-    const {data} = await axios.get(`http://${this.modemIp}/index.php`, {
-      headers: {Accept: 'text/html,application/xhtml+xml,application/xml',
+    const { data } = await axios.get(`http://${this.modemIp}/index.php`, {
+      headers: {
+        Accept: 'text/html,application/xhtml+xml,application/xml',
       },
     })
     const firmwareVersion = extractFirmwareVersion(data as string)
