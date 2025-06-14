741be0d4623b1b8fbd9e1c2a81b1201cc9518d81
Dobroslav Totev <dev@totev.de>
2025-06-14 09:52:46 +0200

    feat: enhance discover and restart commands with IP flag support
    - Added an IP flag to both the discover and restart commands to allow users to specify the modem's IP address.
    - Updated command examples to reflect the new usage of the IP flag.
    - Refactored the discovery logic to utilize the provided IP address when discovering modems.

--- src/commands/restart.ts
+++ src/commands/restart.ts
@@ -1,52 +1,49 @@
-import {Flags} from '@oclif/core'
+import { Flags } from '@oclif/core';
 
-import Command from '../base-command'
-import {discoverModemLocation, ModemDiscovery} from '../modem/discovery'
-import {modemFactory} from '../modem/factory'
+import Command, { ipFlag } from '../base-command';
+import { discoverModemLocation, DiscoveryOptions, ModemDiscovery } from '../modem/discovery';
+import { modemFactory } from '../modem/factory';
 
 export default class Restart extends Command {
-  static description
-    = 'Restart the router/modem'
+  static description = 'restart the modem/router';
   static examples = [
-    '$ vodafone-station-cli restart -p PASSWORD',
-  ]
+    '$ vodafone-station-cli restart',
+    '$ vodafone-station-cli restart --ip 192.168.100.1',
+  ];
+
   static flags = {
+    ip: ipFlag(),
     password: Flags.string({
       char: 'p',
       description: 'router/modem password',
     }),
   }
 
-  async restartRouter(password: string): Promise<unknown> {
-    const modemLocation = await discoverModemLocation()
-    const discoveredModem = await new ModemDiscovery(
-      modemLocation,
-      this.logger,
-    ).discover()
-    const modem = modemFactory(discoveredModem, this.logger)
-    try {
-      await modem.login(password)
-      const restart = await modem.restart()
-      return restart
-    } catch (error) {
-      this.log('Something went wrong.', error)
-      this.error(error as Error)
-    } finally {
-      await modem.logout()
-    }
-  }
-
   async run(): Promise<void> {
     const {flags} = await this.parse(Restart)
-
     const password = flags.password ?? process.env.VODAFONE_ROUTER_PASSWORD
     if (!password || password === '') {
       this.log('You must provide a password either using -p or by setting the environment variable VODAFONE_ROUTER_PASSWORD')
       this.exit()
     }
 
-    this.log('Restarting router... this could take some time...')
-    await this.restartRouter(password!)
+    const discoveryOptions: DiscoveryOptions = {
+      ip: flags.ip,
+    }
+
+    const modemLocation = await discoverModemLocation(discoveryOptions)
+    const discoveredModem = await new ModemDiscovery(modemLocation, this.logger).discover()
+    const modem = modemFactory(discoveredModem, this.logger)
+    try {
+      await modem.login(password!)
+      await modem.restart()
+      this.log('The modem has been restarted.')
+    } catch (error) {
+      this.log('Something went wrong.', error)
+    } finally {
+      await modem.logout()
+    }
+
     this.exit()
   }
 }
